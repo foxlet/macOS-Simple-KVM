5deb1f4734c097a2f3af825072e36101c5267d7d
downthecrop <bryce@mathx.ca>
2019-11-13 11:39:26 +0900

    Install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install qemu-kvm qemu virt-manager virt-viewer libvirt-bin qemu-system qemu-utils python3 python3-pip git qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf
+cd ~
+git clone https://github.com/downthecrop/macOS-Simple-KVM
+cd macOS-Simple-KVM
