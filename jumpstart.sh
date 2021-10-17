5c87e881650289f1be186e71c417756371c9baf9
Someone786-alt <76504556+Someone786-alt@users.noreply.github.com>
2021-10-17 08:08:36 +0530

    Update jumpstart.sh

--- jumpstart.sh
+++ jumpstart.sh
@@ -1,39 +1,9 @@
 #!/bin/bash
 
-# jumpstart.sh: Fetches BaseSystem and converts it to a viable format.
-# by Foxlet <foxlet@furcode.co>
+sudo apt-get update
 
-TOOLS=$PWD/tools
+sudo apt-get install python
 
-print_usage() {
-    echo
-    echo "Usage: $0"
-    echo
-    echo " -s, --high-sierra   Fetch High Sierra media."
-    echo " -m, --mojave        Fetch Mojave media."
-    echo " -c, --catalina      Fetch Catalina media."
-    echo
-}
+python fetchBigSur.py
 
-error() {
-    local error_message="$*"
-    echo "${error_message}" 1>&2;
-}
 
-argument="$1"
-case $argument in
-    -h|--help)
-        print_usage
-        ;;
-    -s|--high-sierra)
-        "$TOOLS/FetchMacOS/fetch.sh" -v 10.13 || exit 1;
-        ;;
-    -m|--mojave)
-        "$TOOLS/FetchMacOS/fetch.sh" -v 10.14 || exit 1;
-        ;;
-    -c|--catalina|*)
-        "$TOOLS/FetchMacOS/fetch.sh" -v 10.15 || exit 1;
-        ;;
-esac
-
-"$TOOLS/dmg2img" "$TOOLS/FetchMacOS/BaseSystem/BaseSystem.dmg" "$PWD/BaseSystem.img"
