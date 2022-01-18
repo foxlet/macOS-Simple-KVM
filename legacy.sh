495a70adee8800b180f186b062dc92d3522d556b
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-01-18 07:48:19 -0500

    Create legacy.sh

---
+++ legacy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+OSK="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+VMDIR=$PWD
+OVMF=$VMDIR/firmware
+#export QEMU_AUDIO_DRV=pa
+#QEMU_AUDIO_DRV=pa
+
+qemu-system-x86_64 \
+    -enable-kvm \
+    -m 2G \
+    -machine q35,accel=kvm \
+    -smp 4,cores=2 \
+    -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
+    -device isa-applesmc,osk="$OSK" \
+    -smbios type=2 \
+    -drive if=pflash,format=raw,readonly,file="$OVMF/OVMF_CODE.fd" \
+    -drive if=pflash,format=raw,file="$OVMF/OVMF_VARS-1024x768.fd" \
+    -vga qxl \
+    -device ich9-intel-hda -device hda-output \
+    -usb -device usb-kbd -device usb-mouse \
+    -netdev user,id=net0 \
+    -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
+    -device ich9-ahci,id=sata \
+    -drive id=ESP,if=none,format=qcow2,file=Clover.qcow2 \
+    -device ide-hd,bus=sata.2,drive=ESP \
+    -drive id=InstallMedia,format=raw,if=none,file=BaseSystem.img \
+    -device ide-hd,bus=sata.3,drive=InstallMedia \
+    -drive id=SystemDisk,if=none,file=macOS.qcow2 \
+    -device ide-hd,bus=sata.4,drive=SystemDisk \
