7001fdf76275647fffa362f969e7e107b22fd247
Someone786-alt <76504556+Someone786-alt@users.noreply.github.com>
2022-01-19 15:42:42 +0530

    Add files via upload

---
+++ fetchOSX.py
@@ -0,0 +1,18 @@
+import os
+
+print("Mojave : 1")
+print("Catalina : 2")
+print("Big Sur : 3")
+print  ("Monterey : 4")
+osxver = input("Enter Version: ")
+
+if osxver == 1:
+  os.system("python macrecovery.py -b Mac-7BA5B2DFE22DDD8C -m 00000000000KXPG00 download")
+elif osxver == 2:
+ os.system(" python macrecovery.py -b Mac-00BE6ED71E35EB86 -m 00000000000000000 download")
+elif osxver == 3:
+ os.system(" python macrecovery.py -b Mac-42FD25EABCABB274 -m 00000000000000000 download")
+else:
+ os.system(" python ./macrecovery.py -b Mac-E43C1C25D4880AD6 -m 00000000000000000 download")
+
+ 
