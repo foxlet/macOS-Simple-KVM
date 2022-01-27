54488abb9e006bf717da1974472135c81d8cdad7
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-01-27 13:12:54 -0500

    made BaseSystem downloaded last

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 sudo apt-get install qemu-system qemu-utils python3 python3-pip -y  # for Ubuntu, Debian, Mint, and PopOS.
 
-./fetch-macOS-v2.py
-
 qemu-img convert BaseSystem.dmg -O raw BaseSystem.img
 
 qemu-img create -f qcow2 macOS.qcow2 64G
+
+./fetch-macOS-v2.py
 
 sudo ./basic.sh
