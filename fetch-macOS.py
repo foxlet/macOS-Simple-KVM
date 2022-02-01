f7e70b226918c92544f6dd4ede5593466a87329a
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-02-01 15:56:13 -0500

    Update fetch-macOS.py

--- fetch-macOS.py
+++ fetch-macOS.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import os
 
 print("Select the version of macOS you want to download")
@@ -18,24 +16,24 @@ osxver = input("Enter Version: ")
 
 
 if osxver == 1:
-  subprocess.call("python3 macrecovery.py -b Mac-2E6FAB96566FE58C -m 00000000000F25Y00 download")
+  subprocess.call("python macrecovery.py -b Mac-2E6FAB96566FE58C -m 00000000000F25Y00 download")
 elif osxver == 2:
-  subprocess.call("python3 macrecovery.py -b Mac-7DF2A3B5E5D671ED -m 00000000000F65100 download")
+  subprocess.call("python macrecovery.py -b Mac-7DF2A3B5E5D671ED -m 00000000000F65100 download")
 elif osxver == 3:
-  subprocess.call("python3 macrecovery.py -b Mac-F60DEB81FF30ACF6 -m 00000000000FNN100 download")
+  subprocess.call("python macrecovery.py -b Mac-F60DEB81FF30ACF6 -m 00000000000FNN100 download")
 elif osxver == 4:
-  subprocess.call("python3 macrecovery.py -b Mac-E43C1C25D4880AD6 -m 00000000000GDVW00 download")
+  subprocess.call("python macrecovery.py -b Mac-E43C1C25D4880AD6 -m 00000000000GDVW00 download")
 elif osxver == 5:
-  subprocess.call("python3 macrecovery.py -b Mac-FFE5EF870D7BA81A -m 00000000000GQRX00 download")
+  subprocess.call("python macrecovery.py -b Mac-FFE5EF870D7BA81A -m 00000000000GQRX00 download")
 elif osxver == 6:
-  subprocess.call("python3 macrecovery.py -b Mac-77F17D7DA9285301 -m 00000000000J0DX00 download")
+  subprocess.call("python macrecovery.py -b Mac-77F17D7DA9285301 -m 00000000000J0DX00 download")
 elif osxver == 7:
-  subprocess.call("python3 macrecovery.py -b Mac-7BA5B2D9E42DDD94 -m 00000000000J80300 download")
+  subprocess.call("python macrecovery.py -b Mac-7BA5B2D9E42DDD94 -m 00000000000J80300 download")
 elif osxver == 8:
-  subprocess.call("python3 macrecovery.py -b Mac-7BA5B2DFE22DDD8C -m 00000000000KXPG00 download")
+  subprocess.call("python macrecovery.py -b Mac-7BA5B2DFE22DDD8C -m 00000000000KXPG00 download")
 elif osxver == 9:
- subprocess.call(" python3 macrecovery.py -b Mac-00BE6ED71E35EB86 -m 00000000000000000 download")
+ subprocess.call(" python macrecovery.py -b Mac-00BE6ED71E35EB86 -m 00000000000000000 download")
 elif osxver == 10:
- subprocess.call(" python3 macrecovery.py -b Mac-42FD25EABCABB274 -m 00000000000000000 download")
+ subprocess.call(" python macrecovery.py -b Mac-42FD25EABCABB274 -m 00000000000000000 download")
 elif osxver == 11:
- subprocess.call(" python python3 macrecovery.py -b Mac-E43C1C25D4880AD6 -m 00000000000000000 download")
+ subprocess.call(" python python macrecovery.py -b Mac-E43C1C25D4880AD6 -m 00000000000000000 download")
