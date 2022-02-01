cb5f0999c32632bad0852c9a64d2403b8c134850
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-02-01 16:05:02 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,17 @@ sudo apt-get install qemu-system qemu-utils python3 python3-pip -y  # for Ubuntu
 
 qemu-img create -f qcow2 macOS.qcow2 64G
 
-./fetch-macOS-v2.py
+python fetch-macOS.py
 
-qemu-img convert BaseSystem.dmg -O raw BaseSystem.img
+if [ -e BaseSystem.dmg ]
+then
+    qemu-img convert BaseSystem.dmg -O raw BaseSystem.img
+else
+    if [ -e RecoveryImage.dmg ]
+then
+    qemu-img convert RecoveryImage.dmg -O raw BaseSystem.img
+else
+    echo "macOS installer not found"
+fi
 
 sudo ./basic.sh
