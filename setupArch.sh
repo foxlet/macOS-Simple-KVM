3de38f687763e251693cdb8832663b0ce3bd3c7b
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-02-03 07:40:54 -0500

    Create setupArch.sh

---
+++ setupArch.sh
@@ -0,0 +1,9 @@
+sudo pacman -S qemu python python3-pip python-wheel
+
+qemu-img create -f qcow2 macOS.qcow2 64G
+
+./fetch-macOS-v2.py
+
+qemu-img convert BaseSystem.dmg -O raw BaseSystem.img
+
+sudo ./basic.sh
